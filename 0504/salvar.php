fa576f505d0485f9e5767d148c56961c5c2252a0
pietronicolettisenacti18 <110640021+pietronicolettisenacti18@users.noreply.github.com>
2023-04-05 20:30:18 -0300

    att

--- 0504/salvar.php
+++ 0504/salvar.php
@@ -3,11 +3,15 @@
 $nome = $_POST['nome'];
 $cidade = $_POST['cidade'];
 $telefone = $_POST['telefone'];
+$estado = $_POST['estado'];
+$email = $_POST['email'];
 
 $arquivo = fopen("clientes.csv", "a");
 
 fwrite($arquivo, $nome .",") ;
 fwrite($arquivo, $cidade .",");
+fwrite($arquivo, $estado .",");
+fwrite($arquivo, $email .",");
 fwrite($arquivo, $telefone ."\n");
 
 fclose($arquivo);
